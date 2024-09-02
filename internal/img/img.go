4664bfbb28818581f8956682bcdb4f1a5030f16a
Bruno Leite <bruno.marinho@useall.com.br>
2024-09-02 17:53:29 -0300

    rect width equals string width + 1 percent

--- internal/img/img.go
+++ internal/img/img.go
@@ -2,6 +2,7 @@ package img
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"image"
 	"image/color"
@@ -9,10 +10,14 @@ import (
 	"image/jpeg"
 	"image/png"
 	"io"
+	"math"
 	"os"
 	"strings"
 
 	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 )
 
 func SaveImage(img *image.RGBA, filepath string) error {
@@ -34,8 +39,21 @@ func LoadImage(reader io.Reader) (image.Image, error) {
 }
 
 func ProcessImage(img image.Image, format string) (io.Reader, error) {
-	newImg := drawRect(img)
-	err := drawText(newImg)
+	var texto = "Hello Teste Teste Teste Teste Teste Teste Teste Teste Teste !"
+	if len(texto) > 110 {
+		return nil, errors.New("maximo de caracteres")
+	}
+	fontBytes, err := os.ReadFile("assets/arial.ttf")
+	if err != nil {
+		return nil, err
+	}
+	rsize, err := measureStringSize(img, texto, fontBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	newImg := drawRect(img, rsize)
+	err = drawText(newImg, texto, fontBytes)
 	if err != nil {
 		return nil, err
 	}
@@ -57,11 +75,33 @@ func ProcessImage(img image.Image, format string) (io.Reader, error) {
 	return buf, nil
 }
 
-func drawText(newImg *image.RGBA) error {
-	fontBytes, err := os.ReadFile("assets/arial.ttf")
+func measureStringSize(img image.Image, text string, fontBytes []byte) (fixed.Int26_6, error) {
+	f, err := truetype.Parse(fontBytes)
 	if err != nil {
-		return err
+		return 0, err
 	}
+
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+
+	newImg := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	ff := truetype.NewFace(f, &truetype.Options{
+		Size: 24,
+		DPI:  72,
+	})
+
+	drawer := font.Drawer{
+		Dst:  newImg,
+		Src:  newImg,
+		Face: ff,
+		Dot:  fixed.Point26_6{},
+	}
+	size := drawer.MeasureString(text)
+	return size, nil
+}
+
+func drawText(newImg *image.RGBA, text string, fontBytes []byte) error {
 	c := freetype.NewContext()
 	font, err := freetype.ParseFont(fontBytes)
 	if err != nil {
@@ -75,21 +115,25 @@ func drawText(newImg *image.RGBA) error {
 	c.SetSrc(image.NewUniform(color.White))
 
 	pt := freetype.Pt(25, newImg.Rect.Dy()-35)
-	_, err = c.DrawString("Hello, World!", pt)
+	_, err = c.DrawString(text, pt)
+
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func drawRect(img image.Image) *image.RGBA {
+func drawRect(img image.Image, size fixed.Int26_6) *image.RGBA {
 	width := img.Bounds().Dx()
 	height := img.Bounds().Dy()
 
 	newImg := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(newImg, newImg.Bounds(), img, image.Point{0, 0}, draw.Over)
 
-	rectW := int(float64(width) * 0.3)
+	wpercent := (float64(width) * 0.3)
+	println(size)
+	println(size.Ceil())
+	rectW := int(math.Max(wpercent, float64(size.Ceil()+int(float64(width)*0.1))))
 	rectH := 60
 	padding := 15
 	rectPos := image.Rect(padding, height-rectH-padding, padding+rectW, height-padding)
