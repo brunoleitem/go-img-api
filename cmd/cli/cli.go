ed460673b2385a2605b63df490ecd33ac84b55ff
Bruno Leite <bruno.marinho@useall.com.br>
2024-09-09 11:25:50 -0300

    fix: cli and examples

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"context"
-	"log"
-
-	"github.com/brunoleitem/go-img-api/internal/r2"
 	"github.com/joho/godotenv"
 )
 
@@ -13,15 +9,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	rt, err := r2.NewR2Service()
-	if err != nil {
-		panic(err)
-	}
-	lala := "6f1a4b2a-1105-41bb-bd13-a26bc612e3d0.jpg"
-	var s *string = &lala
-
-	err = rt.DeleteImage(context.TODO(), s)
-	if err != nil {
-		log.Fatalf("ERRO DELETANDO %v", err)
-	}
+	// TODO CLI
 }
